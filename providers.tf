ab5cd2aef8623f778279cd5a932bbabc34f71e8f
unknown <a.vanzan@reply.it>
2022-06-13 14:44:18 +0000

    Add sample application files

--- providers.tf
+++ providers.tf
@@ -7,3 +7,17 @@ provider "aws" {
   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
