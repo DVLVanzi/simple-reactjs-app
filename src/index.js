3ae41e5366d588cf1ff5d2bed589163772f1858f
unknown <a.vanzan@reply.it>
2022-06-14 14:32:48 +0000

    Add sample application files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+//change 140622 16:30
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
