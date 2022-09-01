9fd7a9db78cbdc1cdfefa40bdb4f1d070610b174
DVLVanzi <31401885+DVLVanzi@users.noreply.github.com>
2022-09-01 17:15:28 +0200

    Add files via upload

--- TestTerraform.tf
+++ TestTerraform.tf
@@ -6,9 +6,23 @@ terraform {
 
   }
 //buckets
-  resource "terraform_test_bucket" "b" {
+  resource "aws_s3_bucket" "example" {
     bucket = "terraform_test_bucket"
     acl    = "private"
+    versioning.mfa_delete = "true"
+    versioning.enabled = "true"
+
+    object_lock_configuration {
+    object_lock_enabled = "Enabled"
+
+    rule {
+      default_retention {
+        mode = "COMPLIANCE"
+        days = 5
+      }
+    }
+  }
+
     tags = {
       Name = "Terraform Test Bucket"
       Owner = "Vanzi"
@@ -16,9 +30,26 @@ terraform {
     }
   }
 
+  resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 //ec2
 
-  resource "terraform_test_ec2" "app_server" {
+  resource "aws_instance" "app_server" {
     ami           = "ami-830c94e3"
     instance_type = "t2.micro"
 
@@ -29,14 +60,14 @@ terraform {
     }
   }
 
-  resource "terraform_test_volume_attachment" "ebs_att" {
+  resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = terraform_test_ebs.test.id
-  instance_id = terraform_test_ec2.app_server.id
+  volume_id   = aws_ebs_volume.test.id
+  instance_id = aws_instance.app_server.id
   }
 
 
-  resource "terraform_test_ebs" "test" {
+  resource "aws_ebs_volume" "test" {
     availability_zone = "us-west-2"
     size              = 1
   }
