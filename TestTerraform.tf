aed64efcd39e4c49c70cb4d96eb8bff837e69a36
DVLVanzi <31401885+DVLVanzi@users.noreply.github.com>
2022-09-01 18:16:34 +0200

    Add files via upload

--- TestTerraform.tf
+++ TestTerraform.tf
@@ -1,47 +1,39 @@
 terraform {
- provider "aws" {
-  alias = "AWS Terraform testing"
-  region  = "us-west-2"
-
-
-  }
-//buckets
-  resource "aws_s3_bucket" "example" {
-    bucket = "terraform_test_bucket"
-    acl    = "private"
-    object_lock_configuration {
-    object_lock_enabled = "Enabled"
-
-    rule {
-      default_retention {
-        mode = "COMPLIANCE"
-        days = 5
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
 
-    tags = {
-      Name = "Terraform Test Bucket"
-      Owner = "Vanzi"
-      BU = "Cloud"
-    }
-  }
-
-  resource "aws_s3_bucket_logging" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
+  required_version = ">= 1.2.0"
 }
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example.bucket
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+//buckets
+//manca logging
+//versioning
+//encryption
+//mfa delete
+//object lock
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-test-bucket-dvlvanzi"
+  
+  tags = {
+    Name  = "terraformtestbucket"
+    Owner = "Vanzi"
+    BU    = "Cloud"
   }
+}
+
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
@@ -50,35 +42,30 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
     status = "Enabled"
   }
 }
-resource "aws_s3_bucket" "test-terraform-mfa-delete" {
-  bucket = aws_s3_bucket.example.id
-  versioning {
-    mfa_delete = true
-  }
-}
 
 //ec2
 
-  resource "aws_instance" "app_server" {
-    ami           = "ami-830c94e3"
-    instance_type = "t2.micro"
+resource "aws_instance" "app_server" {
+  ami           = "ami-0568773882d492fc8"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
 
-    tags = {
-      Name = "TerraformTestingEc2"
-      Owner = "Vanzi"
-      BU = "Cloud"
-    }
+  tags = {
+    Name  = "TerraformTestingEc2"
+    Owner = "Vanzi"
+    BU    = "Cloud"
   }
+}
 
-  resource "aws_volume_attachment" "ebs_att" {
+resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.test.id
   instance_id = aws_instance.app_server.id
-  }
-
-
-  resource "aws_ebs_volume" "test" {
-    availability_zone = "us-west-2"
-    size              = 1
-  }
 }
+
+
+resource "aws_ebs_volume" "test" {
+  availability_zone = "us-east-2a"
+  size              = 1
+}
+
